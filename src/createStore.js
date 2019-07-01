381a6e0c52cad50e863db85318139380aa6505ec
github username <github email address>
2019-06-30 23:57:09 -0400

    set skeleton syntax of createStore function

--- src/createStore.js
+++ src/createStore.js
@@ -2,6 +2,27 @@ export default function createStore(reducer) {
   // add your code here
 }
 
+function createStore(reducer) {
+  let state;
+  function dispatch(action) {
+    state = reducer(state, action)
+    render();
+  };
+
+  function getState() {
+    return state;
+  };
+
+  return {
+    dispatch, getState
+  };
+
+  let store = createStore(countReducer)
+  store.dispatch({type: '@@INIT'})
+
+};  //end of createStore
+
+
 function render() {
   const container = document.getElementById('container');
 }
